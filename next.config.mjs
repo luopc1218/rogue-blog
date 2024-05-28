199682103daf6138c76c8067b2660451fb07477a
rogue.luo <luopc1218@outlook.com>
2024-05-28 09:05:26 +0800

    feat:init.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 允许跨域
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
