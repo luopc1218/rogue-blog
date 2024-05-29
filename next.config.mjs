fde47a88fd58a410e8720878d9f78c65a8fb40a5
rogue.luo <luopc1218@outlook.com>
2024-05-29 15:39:29 +0800

    feat:switch to page router.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 允许跨域
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/:path*",
-        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
